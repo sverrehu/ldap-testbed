4dff79877f9d08ab9d9f98b9eca0923f5bf9c2e8
Sverre H. Huseby <sverre.huseby@statnett.no>
2021-09-21 13:59:24 +0200

    Sync

--- src/main/java/no/shhsoft/ldap/LdapUsernamePasswordAuthenticator.java
+++ src/main/java/no/shhsoft/ldap/LdapUsernamePasswordAuthenticator.java
@@ -56,7 +56,7 @@ implements UsernamePasswordAuthenticator {
             return false;
         }
         if (!StringUtils.isBlank(usernameToUniqueSearchFormat) && originalUsername != null) {
-            populateGroups(context, userDn, originalUsername);
+            populateGroups(context, originalUsername);
         }
         try {
             context.close();
@@ -66,16 +66,16 @@ implements UsernamePasswordAuthenticator {
         return true;
     }
 
-    private void populateGroups(final LdapContext context, final String userDn, final String originalUsername) {
-        UserToGroupsCache.getInstance().fetchGroupsForUserIfNeeded(originalUsername, s -> {
-            if (!s.equals(originalUsername)) {
-                LOG.warn("Expected \"" + originalUsername + "\", but got \"" + s + "\"");
+    private void populateGroups(final LdapContext context, final String username) {
+        UserToGroupsCache.getInstance().fetchGroupsForUserIfNeeded(username, s -> {
+            if (!s.equals(username)) {
+                LOG.warn("Expected \"" + username + "\", but got \"" + s + "\"");
             }
-            return findAdGroups(context, userDn, originalUsername);
+            return findAdGroups(context, username);
         });
     }
 
-    private Set<String> findAdGroups(final LdapContext ldap, final String dn, final String username) {
+    private Set<String> findAdGroups(final LdapContext ldap, final String username) {
         final Set<String> set = new HashSet<>();
         final SearchControls sc = new SearchControls();
         sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
@@ -101,7 +101,7 @@ implements UsernamePasswordAuthenticator {
             }
             return set;
         } catch (final NamingException e) {
-            LOG.warn("Unable to fetch groups for \"" + dn + "\". Will return no groups.", e);
+            LOG.warn("Unable to fetch groups for \"" + username + "\". Will return no groups.", e);
             return Collections.emptySet();
         }
     }
