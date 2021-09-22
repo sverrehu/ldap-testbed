80ebe6c4193938f06a8c5192f304033574f99662
Sverre H. Huseby <sverre.huseby@statnett.no>
2021-09-22 14:42:04 +0200

    Sync

--- src/test/java/no/shhsoft/kafka/auth/container/LdapContainer.java
+++ src/test/java/no/shhsoft/kafka/auth/container/LdapContainer.java
@@ -15,7 +15,7 @@ extends GenericContainer<LdapContainer> {
     public static final String PRODUCER_GROUP = "cn=producers,ou=Groups," + LDAP_BASE_DN;
     public static final String NON_PRODUCER_USER_PASS = "nonproducer";
     public static final String USERNAME_TO_DN_FORMAT = "cn=%s,ou=People,dc=example,dc=com";
-    public static final String USERNAME_TO_UNIQUE_SEARCH_FORMAT = "cn=%s";
+    public static final String USERNAME_TO_UNIQUE_SEARCH_FORMAT = "uid=%s";
 
     public LdapContainer() {
         this(DockerImageName.parse("osixia/openldap:1.4.0"));
@@ -24,6 +24,7 @@ extends GenericContainer<LdapContainer> {
     public LdapContainer(final DockerImageName dockerImageName) {
         super(dockerImageName);
         withClasspathResourceMapping("/ldap/openldap-bootstrap.ldif", "/container/service/slapd/assets/config/bootstrap/ldif/50-openldap-bootstrap.ldif", BindMode.READ_ONLY);
+        withClasspathResourceMapping("/ldap/openldap-access.ldif", "/container/service/slapd/assets/config/bootstrap/ldif/51-openldap-access.ldif", BindMode.READ_ONLY);
         withEnv("LDAP_DOMAIN", LDAP_DOMAIN);
         withEnv("LDAP_BASE_DN", LDAP_BASE_DN);
         withEnv("LDAP_ADMIN_PASSWORD", new String(LDAP_ADMIN_PASSWORD));
